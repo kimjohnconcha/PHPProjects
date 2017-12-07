d37bdbf3ad6ceb4eb3526c7e1f2ad1ea130fc9b5
Kim John Concha <kimjohnconcha@Juponggatana.local>
2017-12-07 21:41:03 +0800

    nothing special

--- index.php
+++ index.php
@@ -1,17 +1,91 @@
-<html>
-<head>
-<title>Guessing game</title>
-</head>
-
-<body>
-
-
 <?php
-  echo "My first PHP script!";
+
+$rand = 0; //rand(1,100);
+$numguess = 0;
+$message = "";
+
+if (isset($_POST["numtobeguessed"])) {
+  $numguess = $_POST["numtobeguessed"];
+  if($numguess == 0)
+    $numguess = rand(1,100);
+    //$rand = rand(1,100);
+}
+
+$count = isset($_POST['count']) ? $_POST['count'] : 0;
+
+if (isset($_POST["number_entered"])) {
+
+  $number= $_POST['number_entered'];
+
+  if($number > $numguess)
+    $message = "Your guess is higher, make it lower!";
+  else if ($number < $numguess) {
+    $message = "Your guess is lower, make it higher!";
+  }
+  else
+    $message = "Congrats!! Your guess is right!! Number to be guess is {$numguess}";
+
+  if ($number > 100 || $number < 0) {
+    $message = "Please guess a number between 1 and 100";
+  }
+
+  $count++;
+
+  if($count == 10)
+  {
+    $message = "Sorry the number to be guessed is {$numguess}. Please try again";
+    $numguess = 0;
+    $count = 0;
+    $number = 0;
+  }
+
+}
+
+
 ?>
 
 
-</body>
+
+<form action="" method="POST">
+
+  Guess a Number Between 1 and 100:
+
+  <input type="text" name="number_entered" value=''/>
+
+  <input type="submit" name="submit" value="Guess"/><br><br>
+
+  <input type="hidden" name="count" value="<?php echo $count; ?>" >
+
+  <input type="hidden" name="numtobeguessed" value="<?php echo $numguess ?>" >
+
+  <?php
+
+  if (isset($_POST["number_entered"])) {
+
+    //$number= $_POST['number_entered'];
+
+    echo "You entered: {$number} ";
+    echo "<br><br>";
+    echo "You tried: {$count} out of 10 ";
+    echo "<br><br>";
+    echo $message;
+  }
+
+  ?>
+  
+</form>
 
 
-</html
+
+
+
+
+
+
+
+
+
+
+
+
+
